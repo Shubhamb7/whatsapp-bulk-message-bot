d2cfc81f93356410bd9729cf2250060797778411
Shubhamb7 <shubhamb756@gmail.com>
2019-01-17 14:08:45 +0530

    updated

--- bot.py
+++ bot.py
@@ -4,10 +4,11 @@ import re
 import selenium
 from tqdm import tqdm
 import pyqrcode
-
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+
+
 options = Options()
 options.set_headless(True)
 driver = webdriver.Firefox(options=options, executable_path=r'/home/shubham/Downloads/geckodriver')
@@ -17,6 +18,8 @@ time.sleep(1)
 token = driver.find_element_by_xpath('//*[@id="app"]/div/div/div[2]/div[1]/div[2]/div').get_attribute('data-ref')
 img = pyqrcode.create(token)
 img.png('QRcode.png',scale=5)
+
+
 try:
     num = int(input('enter number of people you want to send the text to : '))
     msg = input('enter the message : ')
@@ -45,6 +48,8 @@ try:
                 continue       
 except ValueError:
     print('Invalid Input ! ')
+
+
 for i in tqdm(range(0,num)):
     elm = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]')
     driver.execute_script("arguments['0'].innerHTML = '<a href=\"https://api.whatsapp.com/send?phone=+91"+str(no[i])+"&message="+msg+"id=\"contact"+str(i+1)+">"+str(i+1)+"</a>';", elm)
@@ -54,6 +59,8 @@ for i in tqdm(range(0,num)):
     focus = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
     focus.send_keys(msg)
     focus.send_keys(Keys.ENTER)
+    
+
 print('done')
 time.sleep(1)
 dot = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/div')
@@ -61,6 +68,10 @@ dot.click()
 time.sleep(1)
 logout = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/span/div/ul/li[6]/div')
 logout.click()
+
+
 if os.path.exists('QRcode.png'):
     os.remove('QRcode.png')
+
+
 driver.quit()
