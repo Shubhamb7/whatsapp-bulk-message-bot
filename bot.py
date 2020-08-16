20fd529148678ef87e832b8e9c4207d518d42511
Shubham Belgaonkar <shubhamb756@gmail.com>
2020-08-16 17:54:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import time
 import os
 import re
+import PIL
 import selenium
 from tqdm import tqdm
-import pyqrcode
+import qrcode
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -14,9 +15,14 @@ driver = webdriver.Firefox(options=options, executable_path=r'geckodriver.exe')
 driver.get('http://web.whatsapp.com')
 driver.set_window_size(1400,900)
 time.sleep(1)
+
 token = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div[1]/div/div[2]/div').get_attribute('data-ref')
-img = pyqrcode.create(token)
-img.png('QRcode.png',scale=5)
+img = qrcode.make(token)
+size = 300,300
+img.thumbnail(size, PIL.Image.ANTIALIAS)
+img.show()
+time.sleep(6)
+img.close()
 
 try:
     num = int(input('\nenter number of people you want to send the text to : '))
