eca52c60b40aac316a25e5802c6e09f9e8525a6e
Shubham Belgaonkar <shubhamb756@gmail.com>
2020-09-12 19:54:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import time
 import os
 import re
-import PIL
 import selenium
-from tqdm import tqdm
 import qrcode
+import PIL
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -25,16 +24,15 @@ time.sleep(6)
 img.close()
 
 num = int(input('\nenter number of people you want to send the text to : '))
-msg = input('enter the message : ')
+msg = "hello"
 no = []
-
 for i in range(1,num+1):
-    x = int(input("enter number {}: ".format(i)))
+    x = int(input("enter num: "))
     no.append(x)
 
-for i in tqdm(range(0,num)):
+for i in range(0,num):
     elm = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]')
-    driver.execute_script("arguments['0'].innerHTML = '<a href=\"https://api.whatsapp.com/send?phone="+"+91"+str(no[i])+"&message="+msg+"id=\"contact"+str(i+1)+">"+str(i+1)+"</a>';", elm)
+    driver.execute_script(f"arguments['0'].innerHTML = '<a href=\"https://api.whatsapp.com/send?phone=+91{str(no[i])}id=\"contact{str(i+1)}>{str(i+1)}</a>';", elm)
 
     msgele = driver.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/div/div[1]/a")
     msgele.click()
@@ -51,5 +49,6 @@ time.sleep(1)
 logout = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/span/div/ul/li[7]')
 logout.click()
 print('logged out')
+time.sleep(1)
 
 driver.quit()
